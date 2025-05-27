784c930ec4156fde6e6c7a3640f04ec9544ef554
Amelia Johns <arj01638@uga.edu>
2025-05-27 18:38:36 -0400

    debug logging

--- logging_setup.py
+++ logging_setup.py
@@ -4,5 +4,5 @@ def setup_logging():
     logging.basicConfig(
         format='%(asctime)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
-        level=logging.INFO
+        level=logging.DEBUG
     )
